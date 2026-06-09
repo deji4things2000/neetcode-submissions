512968d6396585875aea360361b8ada9a148994c
Dr. Deejay <35232658+deji4things2000@users.noreply.github.com>
2026-06-09 16:41:45 -0400

    Add: best-time-to-buy-and-sell-stock-ii - submission-2

---
+++ Data Structures & Algorithms/best-time-to-buy-and-sell-stock-ii/submission-2.py
@@ -0,0 +1,15 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        n = len(prices)
+
+        prof = prices[0]
+        maxi = 0
+        val = 0
+        
+        for i in range(1, n):
+            diff = prices[i]-prices[i-1]
+            if diff>0:
+                val+=diff
+        return val
+
+        

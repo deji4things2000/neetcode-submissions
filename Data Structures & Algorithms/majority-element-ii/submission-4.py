086e4b1940cd82ab25de0c4703a5de15d2cc70fe
Dr. Deejay <35232658+deji4things2000@users.noreply.github.com>
2026-06-30 14:14:15 -0400

    Add: majority-element-ii - submission-4

---
+++ Data Structures & Algorithms/majority-element-ii/submission-4.py
@@ -0,0 +1,16 @@
+class Solution:
+    def majorityElement(self, nums: List[int]) -> List[int]:
+
+        hm = {}
+        n = len(nums)
+
+        for i in range(n):
+            hm[nums[i]] = hm.get(nums[i], 0) + 1
+        
+        res = []
+        for k,v in hm.items():
+            if v>n//3:
+                res.append(k)
+        return res
+
+        

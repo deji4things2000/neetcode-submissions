4a938a48b157c72c33e5b6e1b45c7a83d97e3346
Dr. Deejay <35232658+deji4things2000@users.noreply.github.com>
2026-07-08 11:39:30 -0400

    Add: count-servers-that-communicate - submission-1

---
+++ Data Structures & Algorithms/count-servers-that-communicate/submission-1.py
@@ -0,0 +1,32 @@
+class Solution:
+    def countServers(self, grid: List[List[int]]) -> int:
+        m, n = len(grid), len(grid[0])
+
+        #dfs is not the approrpriate algorithm here
+
+        row = [0] * m
+        col = [0] * n
+
+        for i in range(m):
+            for j in range(n):
+                if grid[i][j] == 1:
+                    row[i]+=1
+                    col[j]+=1
+        
+
+        res = 0
+
+        for i in range(m):
+            for j in range(n):
+                if grid[i][j] == 1 and (row[i]>1 or col[j] > 1):
+                    res+=1
+        
+        return res
+
+
+
+            
+
+
+
+        

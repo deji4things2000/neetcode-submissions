07d6fe03ca09b868ac15738fcb7293ecfd9d5ef4
Dr. Deejay <35232658+deji4things2000@users.noreply.github.com>
2026-07-08 05:53:38 -0400

    Add: island-perimeter - submission-1

---
+++ Data Structures & Algorithms/island-perimeter/submission-1.py
@@ -0,0 +1,22 @@
+class Solution:
+    def islandPerimeter(self, grid: List[List[int]]) -> int:
+        m, n = len(grid), len(grid[0])
+
+        def dfs(i, j):
+            if i < 0 or j < 0 or i >= m or j >= n or grid[i][j] ==0:
+                return 1
+
+            if grid[i][j] == '#':
+                return 0
+
+            grid[i][j] = '#'
+
+            per = dfs(i-1, j) + dfs(i+1, j) + dfs(i, j-1) + dfs(i, j+1)
+            return per
+
+        for i in range(m):
+            for j in range(n):
+                if grid[i][j] == 1:
+                    return dfs(i, j)
+
+        return 0

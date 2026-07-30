a2e341dbb11c19ec9eff580324339086fa734340
Dr. Deejay <35232658+deji4things2000@users.noreply.github.com>
2026-07-30 20:55:03 +0100

    Add: search-insert-position - submission-4

---
+++ Data Structures & Algorithms/search-insert-position/submission-4.py
@@ -0,0 +1,20 @@
+class Solution:
+    def searchInsert(self, nums: List[int], target: int) -> int:
+        n = len(nums)
+        i, j = 0, n-1
+
+        while i<=j:
+            mid = (i+j)//2
+
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] < target:
+                i = mid + 1
+            else:
+                j = mid - 1
+        return i
+                
+                
+
+            
+        

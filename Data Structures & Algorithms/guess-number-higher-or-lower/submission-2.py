500c15dfd7ee603563079dd6af722451f2ec09a2
Dr. Deejay <35232658+deji4things2000@users.noreply.github.com>
2026-07-30 21:22:44 +0100

    Add: guess-number-higher-or-lower - submission-2

---
+++ Data Structures & Algorithms/guess-number-higher-or-lower/submission-2.py
@@ -0,0 +1,16 @@
+class Solution:
+    def guessNumber(self, n: int) -> int:
+        i, j = 1, n
+
+        while i <= j:  # ← Fixed: use <=
+            mid = (i + j) // 2
+            res = guess(mid)
+            
+            if res == 0:
+                return mid
+            elif res == 1:
+                i = mid + 1
+            else:  # res == 1 (guess is lower than the picked number)
+                j = mid - 1
+        
+        return -1  # Should never reach here

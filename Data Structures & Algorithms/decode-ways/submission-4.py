0a27130cb989d37412db3357ba3256c9917c037b
Dr. Deejay <35232658+deji4things2000@users.noreply.github.com>
2026-08-23 04:42:36 +0100

    Add: decode-ways - submission-4

---
+++ Data Structures & Algorithms/decode-ways/submission-4.py
@@ -0,0 +1,26 @@
+class Solution:
+    def numDecodings(self, s: str) -> int:
+        n = len(s)
+        if not s or s[0] == '0':
+            return 0
+
+        dp = [0] * (n+1)
+        dp[0] = 1
+        dp[1] = 1
+
+        for i in range(2, n+1):
+            #For one digit
+            digit1 = int(s[i-1:i])
+            if 1<=digit1<=9:
+                dp[i] += dp[i-1]
+            
+            #For two digits
+            digit2 = int(s[i-2:i])
+            if 10<=digit2<=26:
+                dp[i] += dp[i-2]
+        return dp[n]
+
+
+
+
+        

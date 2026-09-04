5590bc79fc7c03395d35aa8648b3fcd6cd333d8c
Dr. Deejay <35232658+deji4things2000@users.noreply.github.com>
2026-09-04 01:14:18 +0100

    Add: sort-an-array - submission-17

---
+++ Data Structures & Algorithms/sort-an-array/submission-17.py
@@ -0,0 +1,24 @@
+class Solution:
+    def sortArray(self, nums: List[int]) -> List[int]:
+        def quick(num, left, right):
+            if left>=right:
+                return
+
+            mid = (left+right)//2
+            num[mid], num[left] = num[left], num[mid]
+
+            l = left
+            r = left + 1
+
+            while r<=right:
+                if num[r] < num[left]:
+                    l+=1
+                    num[r], num[l] = num[l], num[r]
+                r+=1
+            
+            num[l], num[left] = num[left], num[l]
+            quick(num, left, l-1)
+            quick(num, l+1, right)
+        
+        quick(nums, 0, len(nums)-1)
+        return nums

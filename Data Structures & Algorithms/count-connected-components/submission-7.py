9149f0682d4625254b12630f518d134ee5617b63
Dr. Deejay <35232658+deji4things2000@users.noreply.github.com>
2026-09-10 13:09:42 +0100

    Add: count-connected-components - submission-7

---
+++ Data Structures & Algorithms/count-connected-components/submission-7.py
@@ -0,0 +1,34 @@
+class Solution:
+    def countComponents(self, n: int, edges: List[List[int]]) -> int:
+        dsu = UnionFind(n)
+        components = n
+
+        for parent, child in edges:
+            if dsu.union(parent,child) == True:
+                components-=1
+        return components
+
+
+class UnionFind:
+    def __init__(self, n):
+        self.size = [1] * n
+        self.parent = list(range(n))
+
+    def find(self, x):
+        if self.parent[x] != x:
+            self.parent[x] = self.find(self.parent[x])
+        return self.parent[x]
+
+    def union(self, x, y):
+        rx = self.find(x)
+        ry = self.find(y)
+
+        if rx == ry:
+            return False
+
+        if self.size[rx] < self.size[ry]:
+            rx, ry = ry, rx
+        
+        self.parent[rx] = ry
+        self.size[rx]+=self.parent[ry]
+        return True 

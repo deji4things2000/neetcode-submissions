bfe78310df0dac884e79d7d4a0ab3ff6de3bce89
Dr. Deejay <35232658+deji4things2000@users.noreply.github.com>
2026-09-20 13:04:45 -0400

    Add: find-the-town-judge - submission-9

---
+++ Data Structures & Algorithms/find-the-town-judge/submission-9.py
@@ -0,0 +1,14 @@
+class Solution:
+    def findJudge(self, n: int, trust: List[List[int]]) -> int:
+        ind = [0] * (n+1)
+        out = [0] * (n+1)
+
+        for trustee, trusted in trust:
+            ind[trusted] +=1
+            out[trustee] +=1
+
+        for i in range(1, n+1):
+            if ind[i] == (n-1) and out[i] == 0:
+                return i
+        return -1
+        
